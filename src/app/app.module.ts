86ee2505c4ef5ae5dfdf763aae70212327788cff
Pankaj <pankajkumarsahu93@gmail.com>
2020-04-07 12:14:46 +0530

    Commit the samplemodule and employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SamplecomponentComponent } from './samplecomponent/samplecomponent.component';
+import { SamplemoduleModule } from './samplemodule/samplemodule.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SamplecomponentComponent } from './samplecomponent/samplecomponent.comp
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SamplemoduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
